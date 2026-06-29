0b67986c15bb2e0cc0d780f8a4119021c93edc0d
Harshey Patnam <56549284+Harshey240@users.noreply.github.com>
2026-06-30 03:12:13 +0530

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.cpp
@@ -0,0 +1,33 @@
+// '-' is a non ascii character
+
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans;
+        for(string s: strs)
+        {
+            ans += s;
+            ans += '-';
+        }
+        return(ans);
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        string temp;
+        for(char c : s)
+        {
+            if(c != '-')
+            {
+                temp += c;
+            }
+            else
+            {
+                ans.push_back(temp);
+                temp = "";
+            }
+        }
+        return(ans);
+    }
+};
